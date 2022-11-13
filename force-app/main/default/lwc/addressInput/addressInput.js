fd0c1bdc087d8dbcfaf556025bda615916a8e554
manasatangudu <118084547+manasatangudu@users.noreply.github.com>
2022-11-12 22:55:58 -0800

    Removed debug logs

--- force-app/main/default/lwc/addressInput/addressInput.js
+++ force-app/main/default/lwc/addressInput/addressInput.js
@@ -1,35 +1,27 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class AddressInput extends LightningElement {
-    @api address = {
-        city: null,
-        street: null,
-        state: null,
-        countryCode: 'US',
-        countryLabel:'United States',
-        postalCode: null
-    };
-    @api addressLabel;
-    _country = 'US';
+  @api address = {
+    city: null,
+    street: null,
+    state: null,
+    countryCode: "US",
+    countryLabel: "United States",
+    postalCode: null
+  };
+  @api addressLabel;
 
-    countryOptions = [{ label: 'United States', value: 'US' }];
+  countryOptions = [{ label: "United States", value: "US" }];
 
-    get getCountryOptions() {
-        return this.countryOptions;
-    }
+  get getCountryOptions() {
+    return this.countryOptions;
+  }
 
-    handleChange(event) {
-        this.address.street = event.detail.street;
-        this.address.city = event.detail.city;
-        this.address.country = event.detail.country;
-        this.address.postalCode = event.detail.postalCode;
-        this.address.state = event.detail.province;
-        console.log('Street => ' , event.target.street);
-        console.log('City => ' , event.target.city);
-        console.log('Province => ' , event.target.province);
-        console.log('Country => ' , event.target.country);
-        console.log('postal Code => ' , event.target.postalCode);
-        console.log('detail country => ' , event.detail.country);
-        this._country = event.detail.country;
-    }
+  handleChange(event) {
+    this.address.street = event.detail.street;
+    this.address.city = event.detail.city;
+    this.address.country = event.detail.country;
+    this.address.postalCode = event.detail.postalCode;
+    this.address.state = event.detail.province;
+  }
 }
