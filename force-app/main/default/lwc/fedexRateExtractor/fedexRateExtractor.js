2d9d2f6020e19293807f913f3850e72d25dfb94a
manasatangudu <118084547+manasatangudu@users.noreply.github.com>
2022-11-12 22:56:36 -0800

    Removed debug logs

--- force-app/main/default/lwc/fedexRateExtractor/fedexRateExtractor.js
+++ force-app/main/default/lwc/fedexRateExtractor/fedexRateExtractor.js
@@ -1,39 +1,42 @@
-import { LightningElement } from 'lwc';
-import getRateOptions from '@salesforce/apex/FedexRateExtractorController.getRateOptions';
-
+import { LightningElement } from "lwc";
+import getRateOptions from "@salesforce/apex/FedexRateExtractorController.getRateOptions";
 
 export default class FedexRateExtractor extends LightningElement {
-    recipient = {};
-    shipper = {};
-    weightValue = 0.0;
-    shippingOptions = [];
-    uiOptions = [];
+  recipient = {};
+  shipper = {};
+  weightValue = 0.0;
+  shippingOptions = [];
+  uiOptions = [];
 
-    getRates(){
-        console.log('weightValue '+this.weightValue);
-       this.shipper= this.template.querySelector("c-address-input[data-my-id=shipper]").address;
-       this.recipient= this.template.querySelector("c-address-input[data-my-id=recipient]").address; 
-        getRateOptions({recipient : JSON.stringify(this.recipient) , shipper : JSON.stringify(this.shipper) , weightValue : this.weightValue})
-        .then(result =>{
-            if(result){
-               this.shippingOptions = JSON.parse(result);
-               this.uiOptions = this.shippingOptions.map(option => ({
-                serviceType: option.serviceType,
-                serviceName: option.serviceName,
-                totalNetCharge : option.ratedShipmentDetails[0].totalNetCharge,
-                totalBaseCharge : option.ratedShipmentDetails[0].totalBaseCharge
-              }));
-            }
-        })
-        .catch(error => {
-            console.log('error: ', error);
-        });
-
-        
-    }
-    handleWeight(event){
-        this.weightValue = event.detail.value;
-        console.log('weight'+this.weightValue);
-    }
-    
+  getRates() {
+    this.shipper = this.template.querySelector(
+      "c-address-input[data-my-id=shipper]"
+    ).address;
+    this.recipient = this.template.querySelector(
+      "c-address-input[data-my-id=recipient]"
+    ).address;
+    getRateOptions({
+      recipient: JSON.stringify(this.recipient),
+      shipper: JSON.stringify(this.shipper),
+      weightValue: this.weightValue
+    })
+      .then((result) => {
+        if (result) {
+          this.shippingOptions = JSON.parse(result);
+          this.uiOptions = this.shippingOptions.map((option) => ({
+            serviceType: option.serviceType,
+            serviceName: option.serviceName,
+            totalNetCharge: option.ratedShipmentDetails[0].totalNetCharge,
+            totalBaseCharge: option.ratedShipmentDetails[0].totalBaseCharge
+          }));
+        }
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
+  }
+  handleWeight(event) {
+    this.weightValue = event.detail.value;
+    console.log("weight" + this.weightValue);
+  }
 }
